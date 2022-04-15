bbc0a9b897a43e04a6931a907b605a2da85287d3
Nico Hauser <me@tyratox.ch>
2022-04-15 11:16:13 +0200

    Update header-html.php

--- header-html.php
+++ header-html.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="format-detection" content="telephone=no">
+    
     <?php
     if (!function_exists('_wp_render_title_tag')) {
         function theme_slug_render_title()
